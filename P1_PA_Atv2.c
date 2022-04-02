e53d7fd6fabd34318dea43152343f815677ae044
Lucas Nunes <lucasnun3s@ucl.br>
2022-04-02 17:23:51 -0300

    nova ideia

--- P1_PA_Atv2.c
+++ P1_PA_Atv2.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-//ideia inicial: enumerar as letras de acordo com suas teclas e utilizando switch case para contagem 
-int teclas(int cont2=0,cont3=0,cont4=0,cont5=0,cont6=0,cont7=0,cont8=0,cont9=0;){
+//nova ideia: ficaria mais limpo se enumerasse por tecla ao invés de letra. 
+typedef (enum){
+    t2=2,
+    t3,
+    t4,
+    t5,
+    t6,
+    t7,
+    t8,
+    t9
+}tecla;
 
+int teclas(){
+    //criar contadores de cada tecla
+    int ( i, cont2, cont3, cont4, cont5, cont6, cont7, cont8, cont9 ) = (0,0,0,0,0,0,0,0,0);
+    char palavra[50];
+
+    printf("digite a palavra %c\n",palavra);
+    scanf("%c",&palavra);
+
+    //garantir que a palavra esta limitada a 50 caracteres.
+    if(palavra[i]<=(i=50)){
+
+    }
+    
 
 }
