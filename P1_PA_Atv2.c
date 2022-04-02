3dc710aa4c2559334837fe4cb4c011c93456297d
Lucas Nunes <lucasnun3s@ucl.br>
2022-04-02 16:54:04 -0300

    ideia_inicial

---
+++ P1_PA_Atv2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//ideia inicial: enumerar as letras de acordo com suas teclas e utilizando switch case para contagem 
+int teclas(int cont2=0,cont3=0,cont4=0,cont5=0,cont6=0,cont7=0,cont8=0,cont9=0;){
+
+
+}
